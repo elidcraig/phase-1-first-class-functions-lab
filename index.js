c148e6af0a305e7fcf7220325dd75dd351a447b6
Eli Craig <elidcraig@gmail.com>
2022-04-26 09:36:52 -0700

    commit tree 88c88f5e3c149fca7bcc56ae1ea6edc5b427306e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    const firstTwoDrivers = drivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const lastTwoDrivers = drivers.slice(-2, );
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return num * fare;
+    }
+}
+
+function fareDoubler(fare) {
+    const doubles = createFareMultiplier(2);
+    return doubles(fare);
+}
+
+function fareTripler(fare) {
+    const triples = createFareMultiplier(3);
+    return triples(fare);
+}
+
+function selectDifferentDrivers(drivers, choice) {
+    return choice(drivers);
+}
